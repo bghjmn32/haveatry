c9449abb35e7c3f20165a12ad37558519f724bcf
bghjmn32 <lhy970110@163.com>
2017-04-07 13:48:36 +0800

    homework

---
+++ HitNewsPage/HitwhNewspage/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from news_show import views
+
+urlpatterns = [
+    url(r'^$', views.index),
+    url(r'^news_list/$', views.index),
+    url(r'^news_info/([0-9]{1,5})/$', views.info),
+    url(r'^admin/', include(admin.site.urls)),
+]
